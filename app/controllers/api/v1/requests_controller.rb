b1654fc4ca10c33fc884316456ca9fd962c9cafb
mkmckenzie <mkmckenzie91@gmail.com>
2017-05-20 14:16:41 -0400

    add request update and tests

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class RequestsController < ApplicationController
-      # before_action :authenticate_user!
+      before_action :authenticate_user!
 
       def index
         authorize Request
@@ -19,12 +19,21 @@ module Api
         render json: @request
       end
 
+      def update
+        request = Request.find_by(id: params[:id])
+        if request.update(request_params)
+          render json: request
+        else
+          render nothing: true, status: :bad_request
+        end
+      end
+
       private
 
       def request_params
         params.require(:request)
               .permit(:details, :language,
-                      :service_id, :requested_mentor_id)
+                      :service_id, :requested_mentor_id, :assigned_mentor_id)
       end
 
     end
