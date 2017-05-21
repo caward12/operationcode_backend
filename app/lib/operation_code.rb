4e27fd2187e164ff8443ec47d826692ddd159b6c
Chris Mar <cmar@customink.com>
2017-05-21 11:33:46 -0400

    stop using rails secrets for now

--- app/lib/operation_code.rb
+++ app/lib/operation_code.rb
@@ -2,7 +2,7 @@ require 'pathname'
 
 class OperationCode
   def self.fetch_secret_with(name:)
-    secret_path = Pathname.new(Rails.configuration.secret_path) + name.to_s
-    File.read(secret_path).chomp
+    # secret_path = Pathname.new(Rails.configuration.secret_path) + name.to_s
+    # File.read(secret_path).chomp
   end
 end
